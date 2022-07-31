4459a3a11bc003930d439eb5e3a6c6b3df8a9fe1
2egn <0821jec@gmail.com>
2022-07-31 20:15:48 +0900

    base

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();   
+        spriteRenderer = GetComponent<SpriteRenderer>();   
+    }
+
+    void Update()
+    {
+        //slip prevent
+        if(Input.GetButtonUp("Horizontal")) {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.000000000001f, rigid.velocity.y);
+        }
+        //Change Facing direction
+        if(Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        //Max Speed
+        if(rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if(rigid.velocity.x < maxSpeed*(-1))
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+
+    }
+}
